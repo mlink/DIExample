dbf0e4bd089eec72a507fe521d17e40a1f7cc265
Michael Link <michael.link@jamf.com>
2022-07-20 10:40:27 -0500

    Initial Commit

---
+++ DIExample/DIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIExampleApp.swift
+//  DIExample
+//
+//  Created by Michael Link on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
